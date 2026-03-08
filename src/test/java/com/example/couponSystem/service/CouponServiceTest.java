2b3afd5aa54fac4d68c7e820eeb85e5684c67848
DY <deltahackall@gmail.com>
2026-03-08 17:24:18 +0900

    feat : add createTestCoupons test.

---
+++ src/test/java/com/example/couponSystem/service/CouponServiceTest.java
@@ -0,0 +1,32 @@
+package com.example.couponSystem.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.couponSystem.repository.CouponRepository;
+
+@SpringBootTest
+class CouponServiceTest {
+
+    @Autowired
+    private CouponService couponService;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Test
+    @Transactional
+    void createTestCoupons_createsExpectedAmount() {
+        couponRepository.deleteAll();
+
+        couponService.createTestCoupons(100);
+        assertEquals(100, couponRepository.count());
+
+        couponService.createTestCoupons(5);
+        assertEquals(105, couponRepository.count());
+    }
+}
