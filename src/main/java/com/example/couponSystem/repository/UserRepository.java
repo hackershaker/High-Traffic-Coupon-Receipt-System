f7ba4e0d94588dfefd65b4be1d22979cd5bf6d65
DY <deltahackall@gmail.com>
2026-03-10 20:25:47 +0900

    feat : add user filter method
    find by username, if username exitst

--- src/main/java/com/example/couponSystem/repository/UserRepository.java
+++ src/main/java/com/example/couponSystem/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.example.couponSystem.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.couponSystem.domain.User;
+import com.example.couponSystem.domain.Member;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
 
+    boolean existsByUsername(String username);
 }
