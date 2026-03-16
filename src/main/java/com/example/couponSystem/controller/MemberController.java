65f2cfdc4aba06bda3b2733b3e380cc2608bafb0
DY <deltahackall@gmail.com>
2026-03-16 18:37:01 +0900

    feat : add @Valid annotation to check more precise account information when signup

--- src/main/java/com/example/couponSystem/controller/MemberController.java
+++ src/main/java/com/example/couponSystem/controller/MemberController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.couponSystem.dto.MemberRegisterRequest;
 import com.example.couponSystem.service.MemberService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -16,8 +17,8 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/signup")
-        public ResponseEntity<Object> signup(@RequestBody MemberRegisterRequest request) {
-            memberService.registerMember(request);
-            return ResponseEntity.created(null).build();
-        }
+    public ResponseEntity<Object> signup(@Valid @RequestBody MemberRegisterRequest request) {
+        memberService.registerMember(request);
+        return ResponseEntity.created(null).build();
+    }
 }
