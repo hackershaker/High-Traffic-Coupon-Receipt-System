110b573e30d57b04c18c59bcbe12e5195897c1dd
DY <deltahackall@gmail.com>
2026-03-16 19:26:36 +0900

    feat : add constraint in signup request

--- src/main/java/com/example/couponSystem/dto/MemberRegisterRequest.java
+++ src/main/java/com/example/couponSystem/dto/MemberRegisterRequest.java
@@ -1,9 +1,16 @@
 package com.example.couponSystem.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class MemberRegisterRequest {
+    @NotBlank(message = "username is required")
+    @Size(min = 4, max = 50, message = "username must be between 4 and 50 characters")
     private String username;
+
+    @NotBlank(message = "password is required")
+    @Size(min = 8, max = 100, message = "password must be between 8 and 100 characters")
     private String password;
 }
